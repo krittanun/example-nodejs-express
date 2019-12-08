26d03139d1eba7410511028d0002372ec723bf8d
krittanun <krittamax@gmail.com>
2019-12-08 16:57:35 +0700

    add elastic-apm-node change version t2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var apm = require('elastic-apm-node').start({
   // Use if APM Server requires a token
   secretToken: '',
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'http://testAPM:8200'
+  serverUrl: 'http://testapm:8200'
 })
 
 var express = require('express');

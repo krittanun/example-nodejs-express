8f69bc57442b04f1088a3bf019bd6b275e46ad16
krittanun <krittamax@gmail.com>
2019-12-08 16:43:42 +0700

    add elastic-apm-node

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// Add this to the VERY top of the first file loaded in your application
+var apm = require('elastic-apm-node').start({
+  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+  serviceName: 'nginx',
+  // Use if APM Server requires a token
+  secretToken: '',
+  // Set custom APM Server URL (default: http://localhost:8200)
+  serverUrl: 'http://testAPM:8200'
+})
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
